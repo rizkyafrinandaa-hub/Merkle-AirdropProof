3ed6b1cad8f2914d92d4d0ffbb23283870ddff22
Rizky Afrinanda <rizkyafrinandaa@gmail.com>
2025-11-21 11:51:40 +0700

    Create config.js

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,10 @@
+export const CONTRACT_ADDRESSES = {
+    token: "0x...", // Paste dari deployment.json
+    airdrop: "0x..." // Paste dari deployment.json
+};
+
+export const CHAIN_CONFIG = {
+    chainId: "0x539", // 1337 untuk localhost
+    chainName: "Localhost 8545",
+    rpcUrls: ["http://127.0.0.1:8545"]
+};
